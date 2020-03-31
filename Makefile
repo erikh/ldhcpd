8cae6f29eafe938f54b9caa249a3a3f408dd9f12
Erik Hollensbe <github@hollensbe.org>
2020-03-30 19:24:04 -0700

    Move handler to new file since it's kind of wordy
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
 IMAGE_NAME ?= ldhcpd:testing
 CODE_PATH ?= /go/src/code.hollensbe.org/erikh/ldhcpd
+DOCKER_CMD := docker run -it --cap-add NET_ADMIN -e IN_DOCKER=1 -e SETUID=$$(id -u) -e SETGID=$$(id -g) -w $(CODE_PATH) -v ${PWD}/.go-cache:/tmp/go-build-cache -v ${PWD}:$(CODE_PATH) $(IMAGE_NAME)
 
 shell: build
 	mkdir -p .go-cache
-	docker run -it --cap-add NET_ADMIN -e SETUID=$$(id -u) -e SETGID=$$(id -g) -w $(CODE_PATH) -v ${PWD}/.go-cache:/tmp/go-build-cache -v ${PWD}:$(CODE_PATH) $(IMAGE_NAME)
+	$(DOCKER_CMD)	
 
 build:
 	box -t $(IMAGE_NAME) box.rb
@@ -22,3 +23,6 @@ interfaces: clean-interfaces
 	sudo ip link set br0 up
 	for i in veth0 veth1 veth2 veth3; do sudo ip link set dev $$i up; done
 	sudo ip addr add dev veth1 10.0.20.1/24
+
+test:
+	if [ -z "$${IN_DOCKER}" ]; then make build && $(DOCKER_CMD) sudo go test -v ./...; else sudo go test -v ./...; fi
--- dhcpd/service.go
+++ dhcpd/service.go
@@ -2,56 +2,10 @@ package dhcpd
 
 import (
 	"fmt"
-	"net"
 	"time"
 
 	"github.com/krolaw/dhcp4"
-	"github.com/pkg/errors"
 )
-
-// Handler is the dhpcd handler for serving requests.
-type Handler struct {
-	ip      net.IP
-	options dhcp4.Options
-	config  Config
-}
-
-// NewHandler creates a new dhcpd handler.
-func NewHandler(interfaceName, configFile string) (*Handler, error) {
-	config, err := ParseConfig(configFile)
-	if err != nil {
-		return nil, errors.Wrap(err, "while loading configuation")
-	}
-
-	intf, err := net.InterfaceByName(interfaceName)
-	if err != nil {
-		return nil, errors.Wrap(err, "error locating interface")
-	}
-
-	addrs, err := intf.Addrs()
-	if err != nil {
-		return nil, errors.Wrap(err, "error locating interface addresses")
-	}
-
-	if len(addrs) != 1 {
-		return nil, errors.New("interface must be configured with exactly one address (for now)")
-	}
-
-	ip, ok := addrs[0].(*net.IPNet)
-	if !ok {
-		return nil, errors.New("internal error resolving interface address")
-	}
-
-	return &Handler{
-		ip:     ip.IP,
-		config: config,
-		options: dhcp4.Options{
-			dhcp4.OptionSubnetMask:       ip.Mask,
-			dhcp4.OptionRouter:           config.GatewayIP(),
-			dhcp4.OptionDomainNameServer: config.DNS(),
-		},
-	}, nil
-}
 
 // ServeDHCP returns a dhcp response for a dhcp request.
 func (h *Handler) ServeDHCP(req dhcp4.Packet, msgType dhcp4.MessageType, options dhcp4.Options) dhcp4.Packet {
