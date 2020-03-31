d7586fa10720a6ebb468fc89e4e69d269be3e303
Erik Hollensbe <github@hollensbe.org>
2020-03-31 02:55:16 -0700

    More allocator tests
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- dhcpd/allocator_test.go
+++ dhcpd/allocator_test.go
@@ -3,13 +3,14 @@ package dhcpd
 import (
 	"os"
 	"testing"
+	"time"
 
 	"code.hollensbe.org/erikh/ldhcpd/testutil"
 )
 
 func TestAllocator(t *testing.T) {
 	config := Config{
-		LeaseDuration: defaultLeaseDuration,
+		LeaseDuration: 100 * time.Millisecond,
 		DNSServers: []string{
 			"10.0.0.1",
 			"1.1.1.1",
@@ -55,5 +56,43 @@ func TestAllocator(t *testing.T) {
 
 	if ip.String() == ip2.String() {
 		t.Fatal("Allocator handed out same address twice")
+	}
+
+	time.Sleep(100 * time.Millisecond) // lease duration
+
+	count, err := db.PurgeLeases()
+	if err != nil {
+		t.Fatalf("could not purge leases: %v", err)
+	}
+
+	if count != 2 {
+		t.Fatal("Did not purge all leases!")
+	}
+
+	if _, err := a.Allocate(testutil.FakeMAC, false); err != nil {
+		t.Fatalf("error allocating first ip: %v", err)
+	}
+
+	if _, err := a.Allocate(testutil.FakeMAC2, false); err != nil {
+		t.Fatalf("Could not allocate second mac: %v", err)
+	}
+
+	time.Sleep(100 * time.Millisecond)
+
+	if _, err := a.Allocate(testutil.FakeMAC, true); err != nil {
+		t.Fatalf("error allocating first ip: %v", err)
+	}
+
+	if _, err := a.Allocate(testutil.FakeMAC2, true); err != nil {
+		t.Fatalf("Could not allocate second mac: %v", err)
+	}
+
+	count, err = db.PurgeLeases()
+	if err != nil {
+		t.Fatalf("could not purge leases: %v", err)
+	}
+
+	if count != 0 {
+		t.Fatal("purged a renewed lease")
 	}
 }
